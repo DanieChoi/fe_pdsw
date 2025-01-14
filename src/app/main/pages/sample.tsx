28dcefb141521de167988c3818c496f1218cf743
hyun <32297817+hyunsokstar@users.noreply.github.com>
2025-01-14 17:13:20 +0900

    update for tab menu 영역 분할2

---
+++ src/app/main/pages/sample.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const SmaplePageForMain = (props: Props) => {
+    return (
+        <div>
+           SmaplePageForMain 
+        </div>
+    )
+}
+
+export default SmaplePageForMain
