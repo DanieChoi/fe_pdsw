f615a9bff77ae5222ead75ddb327d62fcd7fef2d
hyun <32297817+hyunsokstar@users.noreply.github.com>
2025-01-17 11:03:10 +0900

    버튼 수정

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,46 +5,6 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-<<<<<<< HEAD
- "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
- {
-   variants: {
-     variant: {
-       default:
-         "rounded-[3px] bg-[#56CAD6] text-white text-[13px] hover:bg-[#56CAD6]/90",
-       destructive:
-         "rounded-md bg-destructive text-destructive-foreground hover:bg-destructive/90",
-       outline:
-         "rounded-md border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-       secondary:
-         "rounded-md bg-secondary text-secondary-foreground hover:bg-secondary/80",
-       ghost: 
-         "bg-transparent hover:bg-gray-100",
-       link: 
-         "text-primary underline-offset-4 hover:underline",
-       menu:
-         "bg-white hover:bg-gray-50 rounded-md min-w-[76px] py-1.5 h-auto flex flex-col items-center justify-center space-y-1",
-       menuActive:
-         "relative bg-[#E5F3F3] text-[#56CAD6] hover:bg-[#E5F3F3] font-medium rounded-md min-w-[76px] py-1.5 h-auto flex flex-col items-center justify-center space-y-1",
-       menuOpened:
-         "bg-[#E5F3F3]/50 hover:bg-[#E5F3F3]/70 rounded-md min-w-[76px] py-1.5 h-auto flex flex-col items-center justify-center space-y-1",
-       login: 
-         'w-full h-12 bg-black hover:bg-[#55BEC8] text-white text-sm rounded-none',
-     },
-     size: {
-       default: "h-9 px-4",
-       sm: "h-8 px-3 text-xs",
-       lg: "h-10 px-8",
-       icon: "h-9 w-9",
-       none: "",
-     },
-   },
-   defaultVariants: {
-     variant: "default",
-     size: "default",
-   },
- }
-=======
   "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
@@ -82,7 +42,6 @@ const buttonVariants = cva(
       size: "default",
     },
   }
->>>>>>> 1f2f76a576a14a5bdae9f2a6a0657996418e5146
 )
 
 export interface ButtonProps
