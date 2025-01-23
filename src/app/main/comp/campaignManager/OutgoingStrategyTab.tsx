1f503ad6a2da88c35b962f699e1368d6b773d287
seungyoung1 <your-email@example.com>
2025-01-24 08:45:48 +0900

    캠페인관리 발신전략 조회 기능 적용

--- src/app/main/comp/campaignManager/OutgoingStrategyTab.tsx
+++ src/app/main/comp/campaignManager/OutgoingStrategyTab.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TitleWrap from "@/components/shared/TitleWrap";
 import { CommonButton } from "@/components/shared/CommonButton";
 import DataGrid from 'react-data-grid';
 import type { Column } from 'react-data-grid';
+import { MainDataResponse } from '@/features/auth/types/mainIndex';
+import { OutgoingStrategyTabParam } from './CampaignManagerDetail';
 
 interface MaxCallsRow {
   call1: number;
@@ -26,6 +28,8 @@ interface MainRow {
   duration5: number;
   count6: number;
   duration6: number;
+  count7: number;
+  duration7: number;
 }
 
 // Column 타입을 확장하여 children 속성 추가
@@ -47,7 +51,22 @@ const EditableCell = ({ row, column, onRowChange }: any) => {
   );
 };
 
-const OutgoingStrategyTab = () => {
+const CampaignOutgoingOrderTab:OutgoingStrategyTabParam = {
+  changeYn: false,
+  campaignInfoChangeYn: false,
+  onSave: false,
+  onClosed: false,
+  onInit: false,
+  redial_strategy: []
+};
+
+type Props = {
+  campaignInfo: MainDataResponse;
+  onCampaignOutgoingStrategyChange: (param:OutgoingStrategyTabParam) => void;
+};
+
+const OutgoingStrategyTab: React.FC<Props> = ({ campaignInfo, onCampaignOutgoingStrategyChange }) => {
+  const [tempOutgoingStrategyTab, setTempOutgoingStrategyTab] = useState<OutgoingStrategyTabParam>(CampaignOutgoingOrderTab);
   const [maxCallsRows, setMaxCallsRows] = useState<MaxCallsRow[]>([
     {
       call1: 3,
@@ -59,11 +78,11 @@ const OutgoingStrategyTab = () => {
   ]);
 
   const [rows, setRows] = useState<MainRow[]>([
-    { id: '전화번호1', count1: 2, duration1: 0, count2: 1, duration2: 0, count3: 1, duration3: 0, count4: 1, duration4: 0, count5: 1, duration5: 0, count6: 1, duration6: 0 },
-    { id: '전화번호2', count1: 1, duration1: 0, count2: 1, duration2: 0, count3: 1, duration3: 0, count4: 1, duration4: 0, count5: 1, duration5: 0, count6: 1, duration6: 0 },
-    { id: '전화번호3', count1: 1, duration1: 0, count2: 1, duration2: 0, count3: 1, duration3: 0, count4: 1, duration4: 0, count5: 1, duration5: 0, count6: 1, duration6: 0 },
-    { id: '전화번호4', count1: 1, duration1: 0, count2: 1, duration2: 0, count3: 1, duration3: 0, count4: 1, duration4: 0, count5: 1, duration5: 0, count6: 1, duration6: 0 },
-    { id: '전화번호5', count1: 1, duration1: 0, count2: 1, duration2: 0, count3: 1, duration3: 0, count4: 1, duration4: 0, count5: 1, duration5: 0, count6: 1, duration6: 0 }
+    { id: '전화번호1', count1: 2, duration1: 0, count2: 1, duration2: 0, count3: 1, duration3: 0, count4: 1, duration4: 0, count5: 1, duration5: 0, count6: 1, duration6: 0, count7: 1, duration7: 0 },
+    { id: '전화번호2', count1: 1, duration1: 0, count2: 1, duration2: 0, count3: 1, duration3: 0, count4: 1, duration4: 0, count5: 1, duration5: 0, count6: 1, duration6: 0, count7: 1, duration7: 0 },
+    { id: '전화번호3', count1: 1, duration1: 0, count2: 1, duration2: 0, count3: 1, duration3: 0, count4: 1, duration4: 0, count5: 1, duration5: 0, count6: 1, duration6: 0, count7: 1, duration7: 0 },
+    { id: '전화번호4', count1: 1, duration1: 0, count2: 1, duration2: 0, count3: 1, duration3: 0, count4: 1, duration4: 0, count5: 1, duration5: 0, count6: 1, duration6: 0, count7: 1, duration7: 0 },
+    { id: '전화번호5', count1: 1, duration1: 0, count2: 1, duration2: 0, count3: 1, duration3: 0, count4: 1, duration4: 0, count5: 1, duration5: 0, count6: 1, duration6: 0, count7: 1, duration7: 0 }
   ]);
 
   const maxCallsColumns: Column<MaxCallsRow>[] = [
@@ -131,15 +150,74 @@ const OutgoingStrategyTab = () => {
         { key: 'duration6', name: '긴급',  renderCell: EditableCell },
       ],
     },
+    {
+      key: 'section7',
+      name: '채널오류',
+      children: [
+        { key: 'count7', name: '횟수', renderCell: EditableCell },
+        { key: 'duration7', name: '긴급',  renderCell: EditableCell },
+      ],
+    },
   ];
 
   const handleMaxCallsRowsChange = (newRows: MaxCallsRow[]) => {
     setMaxCallsRows(newRows);
+    const tempdata = newRows[0].call1+','+newRows[0].call2+','+newRows[0].call3+','+newRows[0].call4+','+newRows[0].call5
+    setTempOutgoingStrategyTab({...tempOutgoingStrategyTab      
+        , changeYn: true
+        , campaignInfoChangeYn: true
+        , redial_strategy: tempOutgoingStrategyTab.redial_strategy.map((val,index) => tempdata.split(',')[index]+val.substring(val.indexOf(':')))
+    });
   };
 
   const handleMainRowsChange = (newRows: MainRow[]) => {
     setRows(newRows);
+    const tempRedialStrategy = newRows[0].count1+'.'+newRows[0].duration1+'\/3.' 
+    + newRows[0].count2+'.'+newRows[0].duration2 + '\/4.'
+    + newRows[0].count3+'.'+newRows[0].duration3 + '\/5.'
+    + newRows[0].count4+'.'+newRows[0].duration4 + '\/6.'
+    + newRows[0].count5+'.'+newRows[0].duration5 + '\/10.'
+    ;
+    setTempOutgoingStrategyTab({...tempOutgoingStrategyTab      
+        , changeYn: true
+        , campaignInfoChangeYn: true
+        // , redial_strategy: tempOutgoingStrategyTab.redial_strategy.map((val,index) => tempdata.split(',')[index]+val.substring(val.indexOf(':')))
+    });
   };
+
+  useEffect(() => {
+    if (campaignInfo.redial_strategy && campaignInfo.redial_strategy.length > 0) {  
+      setTempOutgoingStrategyTab({...tempOutgoingStrategyTab
+        ,redial_strategy : campaignInfo.redial_strategy
+      }); 
+      setMaxCallsRows(maxCallsRows.map(row => ({
+        ...row,
+        call1: Number(campaignInfo.redial_strategy[0].substring(0, 1)),
+        call2: Number(campaignInfo.redial_strategy[1].substring(0, 1)),
+        call3: Number(campaignInfo.redial_strategy[2].substring(0, 1)),
+        call4: Number(campaignInfo.redial_strategy[3].substring(0, 1)),
+        call5: Number(campaignInfo.redial_strategy[4].substring(0, 1))
+      })));
+      setRows(rows.map((row, index) => ({
+        ...row,
+        id: '전화번호'+(index+1),
+        count1: Number(campaignInfo.redial_strategy[index].split('\/')[0].split('.')[1]),
+        duration1: Number(campaignInfo.redial_strategy[index].split('\/')[0].split('.')[2]),
+        count2: Number(campaignInfo.redial_strategy[index].split('\/')[1].split('.')[1]),
+        duration2: Number(campaignInfo.redial_strategy[index].split('\/')[1].split('.')[2]),
+        count3: Number(campaignInfo.redial_strategy[index].split('\/')[2].split('.')[1]),
+        duration3: Number(campaignInfo.redial_strategy[index].split('\/')[2].split('.')[2]),
+        count4: Number(campaignInfo.redial_strategy[index].split('\/')[3].split('.')[1]),
+        duration4: Number(campaignInfo.redial_strategy[index].split('\/')[3].split('.')[2]),
+        count5: Number(campaignInfo.redial_strategy[index].split('\/')[4].split('.')[1]),
+        duration5: Number(campaignInfo.redial_strategy[index].split('\/')[4].split('.')[2]),
+        count6: Number(campaignInfo.redial_strategy[index].split('\/')[5].split('.')[1]),
+        duration6: Number(campaignInfo.redial_strategy[index].split('\/')[5].split('.')[2]),
+        count7: Number(campaignInfo.redial_strategy[index].split('\/')[6].split('.')[1]),
+        duration7: Number(campaignInfo.redial_strategy[index].split('\/')[6].split('.')[2]),
+      })));
+    }
+  }, [campaignInfo]);
 
   return (
     <div className="py-5">
