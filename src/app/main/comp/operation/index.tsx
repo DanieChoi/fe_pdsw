dcc4f0fb6a5d15f44532702aa983ff25956f6cd8
jisun <time2_go@naver.com>
2025-01-24 16:25:32 +0900

    상담 결과 코드 설정 화면 적용

--- src/app/main/comp/operation/index.tsx
+++ src/app/main/comp/operation/index.tsx
@@ -3,7 +3,9 @@
 import React, { useState } from 'react'
 import CampaignLayout from './CampaignNumberChange/CampaignLayout'
 import EditDescription from './NumberEditDescription/EditDescription'
-import CallLimitSetting from './CallLimitSetting/'
+import CallLimitSetting from './CallLimitSetting'
+import DistributionLimit from './DistributionLimit'
+import ConsultResultSetting from './ConsultResultSetting'
 import Image from 'next/image'
 
 export default function OperationBoard() {
@@ -28,9 +30,9 @@ export default function OperationBoard() {
     { id: 'section1', title: '캠페인별 발신번호 변경', content: <CampaignLayout /> },
     { id: 'section2', title: '전화번호별 설명 편집', content: <EditDescription/> },
     { id: 'section3', title: '예약콜 제한 설정', content: <CallLimitSetting/> },
-    { id: 'section4', title: '분배호수 제한 설정', content: '화면4' },
+    { id: 'section4', title: '분배호수 제한 설정', content: '화면4'},
     { id: 'section5', title: '스킬편집', content: '화면5' },
-    { id: 'section6', title: '상담 결과코드 설정', content: '화면6' },
+    { id: 'section6', title: '상담 결과코드 설정', content: <ConsultResultSetting/> },
     { id: 'section7', title: '서스팬드', content: '화면7' },
   ]
 
