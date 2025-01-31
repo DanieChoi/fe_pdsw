d423c2ed63217cecccf6b98fde2daeac018fc1d0
seungyoung1 <your-email@example.com>
2025-01-31 16:10:54 +0900

    캠페인관리 콜페이싱 탭 적용

--- src/app/main/comp/campaignManager/CallPacingTab.tsx
+++ src/app/main/comp/campaignManager/CallPacingTab.tsx
@@ -3,63 +3,83 @@ import React, { useState, useEffect } from 'react';
 import { CommonButton } from "@/components/shared/CommonButton";
 import TitleWrap from "@/components/shared/TitleWrap";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/shared/CustomSelect";
-import { MainDataResponse } from '@/features/auth/types/mainIndex';
 import { CallPacingTabParam } from './CampaignManagerDetail';
 
 const tempCallPacingTab: CallPacingTabParam = {
   changeYn: false,
-  campaignInfoChangeYn: false,
+  campaignDialSpeedChangeYn: false,
   onSave: false,
   onClosed: false,
-  dial_speed: 0, // Add the missing dial_speed property
+  dial_mode: 0,
+  progressive_dial_speed: 0,
+  predictive_dial_speed: 0,
 };
 
 type Props = {
-  campaignInfo: MainDataResponse;
+  campaignDialSpeedInfo: CallPacingTabParam;
   onHandleCallPacingTabChange: (param:CallPacingTabParam) => void;
 };
 
-const CallPacingTab: React.FC<Props> = ({ campaignInfo, onHandleCallPacingTabChange }) => {
-  const [predictiveValue, setPredictiveValue] = useState(0);
+const CallPacingTab: React.FC<Props> = ({ campaignDialSpeedInfo, onHandleCallPacingTabChange }) => {
+  const [predictiveValue, setPredictiveValue] = useState(50);
   const [progressiveValue, setProgressiveValue] = useState(500);
   const [predictiveUnit, setPredictiveUnit] = useState(1);
   const [progressiveUnit, setProgressiveUnit] = useState(100);
   const [predictiveDisabled, setPredictiveDisabled] = useState(true);
   const [progressiveValueeDisabled, setProgressiveValueDisabled] = useState(true);
+  const [tempCallPacingTabParam, setTempCallPacingTabParam] = useState<CallPacingTabParam>(campaignDialSpeedInfo);
 
-  const units = [1, 5, 10, 50, 100];
+  const predictiveUnits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const progressiveUnits = [1, 5, 10, 50, 100];
 
   const handleDecrement = (type: 'predictive' | 'progressive') => {
     if (type === 'predictive') {
-      setPredictiveValue(Math.max(0, predictiveValue - predictiveUnit));
+      setPredictiveValue(Math.max(50, predictiveValue - predictiveUnit));
+      onHandleCallPacingTabChange({...tempCallPacingTabParam
+        , campaignDialSpeedChangeYn: true
+        , predictive_dial_speed: Math.max(50, predictiveValue - predictiveUnit)
+      });
     } else {
-      setProgressiveValue(Math.max(0, progressiveValue - progressiveUnit));
+      setProgressiveValue(Math.max(25, progressiveValue - progressiveUnit));
+      onHandleCallPacingTabChange({...tempCallPacingTabParam
+        , campaignDialSpeedChangeYn: true
+        , progressive_dial_speed: Math.max(12.5, (progressiveValue - progressiveUnit)/2)
+      });
     }
   };
 
   const handleIncrement = (type: 'predictive' | 'progressive') => {
     if (type === 'predictive') {
-      setPredictiveValue(Math.min(50, predictiveValue + predictiveUnit));
+      setPredictiveValue(Math.min(100, predictiveValue + predictiveUnit));
+      onHandleCallPacingTabChange({...tempCallPacingTabParam
+        , campaignDialSpeedChangeYn: true
+        , predictive_dial_speed: Math.min(100, predictiveValue + predictiveUnit)
+      });
     } else {
       setProgressiveValue(Math.min(500, progressiveValue + progressiveUnit));
+      onHandleCallPacingTabChange({...tempCallPacingTabParam
+        , campaignDialSpeedChangeYn: true
+        , progressive_dial_speed: Math.min(250, (progressiveValue + progressiveUnit)/2)
+      });
     }
   };
 
   useEffect(() => {
-    if (campaignInfo && campaignInfo.campaign_id !== 0) {  
-      if( campaignInfo.dial_mode === 2 ){
+    if (campaignDialSpeedInfo && campaignDialSpeedInfo.dial_mode !== 0) {  
+      if( campaignDialSpeedInfo.dial_mode === 2 ){
         setPredictiveDisabled(true);
         setProgressiveValueDisabled(false);
-      }else if( campaignInfo.dial_mode === 3 ){
+      }else if( campaignDialSpeedInfo.dial_mode === 3 ){
         setPredictiveDisabled(false);
         setProgressiveValueDisabled(true);
       }else{
         setPredictiveDisabled(true);
         setProgressiveValueDisabled(true);
       }
-      
+      setProgressiveValue( campaignDialSpeedInfo.progressive_dial_speed * 2 );
+      setPredictiveValue( campaignDialSpeedInfo.predictive_dial_speed < 50?50:campaignDialSpeedInfo.predictive_dial_speed );
     }
-  }, [campaignInfo]);
+  }, [campaignDialSpeedInfo]);
 
   return (
     <div className="py-5">
@@ -72,7 +92,7 @@ const CallPacingTab: React.FC<Props> = ({ campaignInfo, onHandleCallPacingTabCha
           <div className="border border-[#ebebeb] rounded-[3px] px-[40px] py-[20px] flex flex-col gap-[14px]">
             <div className="flex items-center justify-between">
               <span className="text-sm font-medium">발신 속도 조절</span>
-              <span className="flex items-center"><span className="text-sm font-medium min-w-[25px] text-center">{predictiveValue}</span><span className="text-sm font-medium">(0)</span></span>
+              <span className="flex items-center"><span className="text-sm font-medium min-w-[25px] text-center">{predictiveValue}</span><span className="text-sm font-medium">({predictiveValue === 50?0:'+'+(predictiveValue-50)})</span></span>
               <div className="flex items-center gap-[5px]">
                 <span className="text-sm font-medium">변경 단위 :</span>
                 <Select value={predictiveUnit.toString()} onValueChange={(value) => setPredictiveUnit(Number(value))} disabled={predictiveDisabled}>
@@ -80,7 +100,7 @@ const CallPacingTab: React.FC<Props> = ({ campaignInfo, onHandleCallPacingTabCha
                     <SelectValue placeholder="Select unit" />
                   </SelectTrigger>
                   <SelectContent>
-                    {units.map((unit) => (
+                    {predictiveUnits.map((unit) => (
                       <SelectItem key={unit} value={unit.toString()}>
                         {unit}
                       </SelectItem>
@@ -94,7 +114,7 @@ const CallPacingTab: React.FC<Props> = ({ campaignInfo, onHandleCallPacingTabCha
                 <div className="h-[20px] w-full rounded-full bg-[#ddd]">
                   <div
                     className="h-full rounded-full bg-[#4EE781]"
-                    style={{ width: `${(predictiveValue / 50) * 100}%` }}
+                    style={{ width: `${((predictiveValue-50)*2 / 100) * 100}%` }}
                   />
                 </div>
               </div>
@@ -135,7 +155,7 @@ const CallPacingTab: React.FC<Props> = ({ campaignInfo, onHandleCallPacingTabCha
                     <SelectValue placeholder="Select unit" />
                   </SelectTrigger>
                   <SelectContent>
-                    {units.map((unit) => (
+                    {progressiveUnits.map((unit) => (
                       <SelectItem key={unit} value={unit.toString()}>
                         {unit}
                       </SelectItem>
@@ -149,7 +169,7 @@ const CallPacingTab: React.FC<Props> = ({ campaignInfo, onHandleCallPacingTabCha
                 <div className="h-[20px] w-full rounded-full bg-[#ddd]">
                   <div
                     className="h-full rounded-full bg-green-400"
-                    style={{ width: `${(progressiveValue / 500) * 100}%` }}
+                    style={{ width: `${((progressiveValue - 25) / (500 - 25)) * 100}%` }}
                   />
                 </div>
               </div>
@@ -175,8 +195,16 @@ const CallPacingTab: React.FC<Props> = ({ campaignInfo, onHandleCallPacingTabCha
 
       </div>
       <div className="flex justify-end gap-2 mt-5">
-        <CommonButton variant="secondary">확인</CommonButton>
-        <CommonButton variant="secondary">취소</CommonButton>
+        <CommonButton variant="secondary" onClick={()=> 
+          onHandleCallPacingTabChange({...tempCallPacingTabParam
+            , onSave: true
+          })
+        }>확인</CommonButton>
+        <CommonButton variant="secondary" onClick={()=> 
+          onHandleCallPacingTabChange({...tempCallPacingTabParam
+            , onClosed: true
+          })
+        }>취소</CommonButton>
       </div>
     </div>
   );
