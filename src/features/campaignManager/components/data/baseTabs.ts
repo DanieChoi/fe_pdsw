8a7a9974f14824de2a759d9043121b93a36f6d68
hyun <32297817+hyunsokstar@users.noreply.github.com>
2025-02-03 17:20:32 +0900

    상담원 목록 => 캠페인 목록

--- src/features/campaignManager/components/data/baseTabs.ts
+++ src/features/campaignManager/components/data/baseTabs.ts
@@ -2,7 +2,7 @@
 export const baseTabs = [
     { id: 'campaign', label: '캠페인' },
     { id: 'agent', label: '상담원' },
-    { id: 'agent-group', label: '상담원 그룹' }
+    { id: 'agent-group', label: '캠페인 그룹' }
   ] as const;
   
   export type TabId = typeof baseTabs[number]['id'];
