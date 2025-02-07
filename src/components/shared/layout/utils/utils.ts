e57dad120518da2e945d349262294819712ec710
hyun <32297817+hyunsokstar@users.noreply.github.com>
2025-02-07 18:53:19 +0900

    update for 시작 상태 구분3

--- src/components/shared/layout/utils/utils.ts
+++ src/components/shared/layout/utils/utils.ts
@@ -76,10 +76,7 @@ export const processTreeItemsForSidebar = (items: TreeItem[], type: FilterType):
     .filter((item) => {
       if (type === 'all') return true;
       if (item.type === 'folder' && item.children && item.children.length > 0) return true;
-      if (type === 'active' || type === 'inactive') {
-        return item.status === type;
-      }
-      return false;
+      return item.direction === type;
     });
 };
 
@@ -110,17 +107,32 @@ export const sortTreeItemsForSidebar = (items: TreeItem[], type: SortType): Tree
 // if (start_flag === 1) return 'started';      // 종료된 상태
 // if (start_flag === 2) return 'pending';    // 대기 상태
 // if (start_flag === 3) return 'stopped';     // 진행중 
-export const getStatusIcon = (start_flag?: number): string | null => {
-  switch (start_flag) {
-    case 1:
-      return 'started'; // 종료된 상태
-    case 2:
-      return 'pending'; // 대기 상태
-    case 3:
-      return 'stopped'; // 진행 중
-    default:
-      return null;
-  }
-};
+
+// export const getStatusIcon = (start_flag?: number): string | null => {
+//   switch (start_flag) {
+//     case 1:
+//       return 'started'; // 종료된 상태
+//     case 2:
+//       return 'pending'; // 대기 상태
+//     case 3:
+//       return 'stopped'; // 진행 중
+//     default:
+//       return null;
+//   }
+// };
+
+
+export const getStatusIcon = (status?: string) => {
+    switch (status) {
+      case 'started':
+        return '/sidebar-menu/tree_play.svg';
+      case 'pending':
+        return '/sidebar-menu/tree_pause.svg';
+      case 'stopped':
+        return '/sidebar-menu/tree_stop.svg';
+      default:
+        return null;
+    }
+  };
 
   
