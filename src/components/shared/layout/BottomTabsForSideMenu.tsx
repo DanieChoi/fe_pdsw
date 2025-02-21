580b1b410177d4d8bc34d4ba7b1a154638cdf581
jisun <time2_go@naver.com>
2025-02-21 11:24:28 +0900

    하단 캠페인, 상담원, 상담원 그룹  아이콘 수정

--- src/components/shared/layout/BottomTabsForSideMenu.tsx
+++ src/components/shared/layout/BottomTabsForSideMenu.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { TabId } from "@/features/campaignManager/components/data/baseTabs";
-
+import Image from 'next/image'
 interface BottomTabsForSideMenuProps {
   selectedTabId: TabId;
   onTabChange: (tabId: TabId) => void;
@@ -11,38 +11,58 @@ const tabsConfig = [
   {
     id: "campaign" as TabId,
     label: "캠페인",
-    icon: "/tree-menu/campaign_icon_for_sidemenu.png",
-    alt: "캠페인 아이콘"
+    icons: {
+      default: "/tree-menu/campaign_icon_for_sidemenu.png",
+      selected: "/tree-menu/campaign_icon_for_sidemenu_selected.png"
+    },
+    alt: "캠페인 아이콘",
+    iconWidth: 13,
+    iconHeight: 13
   },
   {
     id: "agent" as TabId,
     label: "상담원",
-    icon: "/tree-menu/ghost_icon_for_counselor_tab.png",
-    alt: "상담원 아이콘"
+    icons: {
+      default: "/tree-menu/ghost_icon_for_counselor_tab.png",
+      selected: "/tree-menu/ghost_icon_for_counselor_tab_selected.png"
+    },
+    alt: "상담원 아이콘",
+    iconWidth: 14,
+    iconHeight: 14
   },
   {
     id: "agent-group" as TabId,
     label: "상담원 그룹",
-    icon: "/tree-menu/campain_group.png",
-    alt: "상담원 그룹 아이콘"
+    icons: {
+      default: "/tree-menu/campain_group.png",
+      selected: "/tree-menu/campain_group_selected.png"
+    },
+    alt: "상담원 그룹 아이콘",
+    iconWidth: 12,
+    iconHeight: 12
   }
 ];
 
 export function BottomTabsForSideMenu({ selectedTabId, onTabChange }: BottomTabsForSideMenuProps) {
   return (
-    <div className="flex-none border-t">
+    <div className="flex-none">
       {tabsConfig.map((tab) => (
         <button
           key={tab.id}
-          className={`w-full text-left px-4 py-2 text-sm font-medium flex items-center gap-2
+          className={`w-full text-left px-4 py-2 text-sm font-medium flex items-center gap-2 border-t
             ${
               selectedTabId === tab.id
-                ? "bg-blue-50 text-blue-600"
+                ? "bg-[#F1FBFC] text-[#02AFBF]"
                 : "text-gray-600 hover:bg-gray-50 hover:text-gray-800"
             }`}
           onClick={() => onTabChange(tab.id)}
         >
-          <img src={tab.icon} alt={tab.alt} className="w-4 h-4" />
+          <Image 
+              src={selectedTabId === tab.id ? tab.icons.selected : tab.icons.default} 
+              alt={tab.alt} 
+              width={tab.iconWidth} 
+              height={tab.iconHeight} 
+            />
           <span>{tab.label}</span>
         </button>
       ))}
