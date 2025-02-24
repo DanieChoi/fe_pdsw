2c08a0e56472a504f9f768173521eb9879dcdbf1
jisun <time2_go@naver.com>
2025-02-24 15:50:39 +0900

    메뉴 슬라이드 버튼 디자인 수정

--- src/components/shared/layout/Sidebar2.tsx
+++ src/components/shared/layout/Sidebar2.tsx
@@ -183,15 +183,13 @@ function SidebarToggleButton({ isOpen, onClick }: SidebarToggleButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="absolute -right-0 top-20 w-7 h-9 flex items-center justify-center
-                bg-white border border-gray-300 rounded-lg shadow-sm
-                hover:bg-gray-100 hover:border-gray-400
+      className="sidebar-button
                 transition-all duration-300 ease-in-out z-30"
     >
       {isOpen ? (
-        <ChevronLeft className="w-5 h-5 text-gray-600" />
+        <ChevronLeft className="w-4 h-4 text-gray-600" />
       ) : (
-        <ChevronRight className="w-5 h-5 text-gray-600" />
+        <ChevronRight className="w-4 h-4 text-gray-600" />
       )}
     </button>
   );
@@ -268,8 +266,7 @@ export default function SidebarContainer() {
       <div 
         className="flex flex-col transition-all duration-300 ease-in-out relative"
         style={{ 
-          width: storeIsOpen ? `${storeWidth}px` : '30px',
-          minWidth: '30px'
+          width: storeIsOpen ? `${storeWidth}px` : '0px',
         }}
       >
         {/* Main Content Area */}
