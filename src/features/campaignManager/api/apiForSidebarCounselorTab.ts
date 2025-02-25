9f9870812b5acef37edd661f7f7bccd5cff2e239
hyun <32297817+hyunsokstar@users.noreply.github.com>
2025-02-25 17:30:10 +0900

    상담원 sort 구현 1

--- src/features/campaignManager/api/apiForSidebarCounselorTab.ts
+++ src/features/campaignManager/api/apiForSidebarCounselorTab.ts
@@ -4,18 +4,6 @@ import { axiosInstance, axiosRedisInstance } from "@/lib/axios";
 import { Counselor, CounselorNode, GroupNode, TabData, TeamNode, TenantNode } from "../types/typeForSideBarCounselorTab";
 import { IParmaterForFetchCounsolorList } from "../types/typeForSideBarCounselorTab2";
 
-// // 상담원 리스트 api 함수 호출2
-// export async function apiForTreeMenuDataForSimeBarCounselorTab(credentials: MainCredentials2) {
-//   const { tenant_id, roleId } = credentials;
-//   const response = await axiosRedisInstance.get(
-//     `/counselor/list?tenantId=${tenant_id}&roleId=${roleId}`
-//   );
-//   return response.data;
-// }
-
-// get
-// http://localhost:4000/api/v1/counselor/list?tenantId=0&roleId=6
-
 export async function apiToFetchCounselorListForSideBar({
   tenant_id,
   roleId
