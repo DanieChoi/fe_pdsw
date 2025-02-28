7b06cc69026e7d19f57b7e87a30c682f9707626f
hyun <32297817+hyunsokstar@users.noreply.github.com>
2025-02-28 14:30:49 +0900

    푸터 사이즈 조절시에 좀더 원활하게 조절 되도록 re-resizable 라이브러리 적용2

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -176,7 +176,6 @@ export default function MainLayout({
             >
               <Footer 
                 footerHeight={footerHeight} 
-                startResizing={startResizing}
                 onToggleDrawer={toggleFooter}
               />
             </div>
