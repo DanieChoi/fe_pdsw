f2c10697a4f5f2ffeb9f62d052abcef08649bcd7
shinwoejin <shinwoejin@naver.com>
2025-03-06 17:28:47 +0900

    운영설정 이벤트키 수정

--- src/app/main/comp/operation/index.tsx
+++ src/app/main/comp/operation/index.tsx
@@ -6,7 +6,7 @@ import EditDescription from './NumberEditDescription/EditDescription'
 import CallLimitSetting from './CallLimitSetting'
 import DistributionLimit from './DistributionLimit'
 import SkillEdit from './SkillEdit'
-import ConsultResultSetting from './ConsultResultSetting'
+// import ConsultResultSetting from './ConsultResultSetting'
 import SuspendView from './SuspendView'
 import Image from 'next/image'
 import { useTabStore } from '@/store'
@@ -27,14 +27,15 @@ export default function OperationBoard() {
     setOpenSectionId(openSectionId === sectionId ? '' : sectionId)
   }
 
+  // 섹션 데이터를 배열로 정의
   const sections = [
-    { id: 'section1', title: '캠페인별 발신번호 변경', content: <CampaignLayout /> },
-    { id: 'section2', title: '전화번호별 설명 편집', content: <EditDescription/> },
-    { id: 'section3', title: '예약콜 제한 설정', content: <CallLimitSetting/> },
-    { id: 'section4', title: '분배호수 제한 설정', content: <DistributionLimit/>},
-    { id: 'section5', title: '스킬편집', content: <SkillEdit/> },
-    // { id: 'section6', title: '상담 결과코드 설정', content: <ConsultResultSetting/> },
-    { id: 'section7', title: '서스팬드', content: <SuspendView/> },
+    { id: 'section1', title: '캠페인별 발신번호 변경', component: CampaignLayout },
+    { id: 'section2', title: '전화번호별 설명 편집', component: EditDescription },
+    { id: 'section3', title: '예약콜 제한 설정', component: CallLimitSetting },
+    { id: 'section4', title: '분배호수 제한 설정', component: DistributionLimit },
+    { id: 'section5', title: '스킬편집', component: SkillEdit },
+    // { id: 'section6', title: '상담 결과코드 설정', component: ConsultResultSetting },
+    { id: 'section7', title: '서스팬드', component: SuspendView },
   ]
 
   return (
@@ -66,7 +67,8 @@ export default function OperationBoard() {
               ${openSectionId === section.id ? 'opacity-100' : 'max-h-0 opacity-0'}`}
           >
             <div className="py-[35px] px-[40px] border-t border-gray-200">
-              {section.content}
+              {/* 컴포넌트를 조건부 렌더링 */}
+              {openSectionId === section.id && React.createElement(section.component)}
             </div>
           </div>
         </div>
