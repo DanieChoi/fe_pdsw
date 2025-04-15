7c4e99a4ddb63bf0947b606ec466ab80bb6c1826
DanieChoi <swchoi317@kakao.com>
2025-04-15 11:25:48 +0900

    [최상원]설정 원복

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
+
 import type { NextConfig } from "next";
 import packageJson from './package.json';
+
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
@@ -8,6 +10,8 @@ const nextConfig: NextConfig = {
     domains: ['localhost', '10.10.40.145'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+
+    // 환경 변수 추가
   },
 
   env: {
@@ -18,11 +22,11 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api_upds/v1/:path*',
-        destination: '${process.env.NEXT_PUBLIC_API_URL}/api_upds/v1/:path*'
+        destination: 'http://10.10.30.228:4000/api_upds/v1/:path*'
       },
       {
         source: '/pds/:path*',
-        destination: '${process.env.NEXT_PUBLIC_RESTAPI_URL}/pds/:path*'
+        destination: 'http://10.10.40.145:8010/pds/:path*'
       }
     ]
   }
