962eb02a2c32a38e46f0da2a3bf644e3eb72b8a5
hyun <32297817+hyunsokstar@users.noreply.github.com>
2025-04-18 17:32:08 +0900

    update for env.js

--- src/app/utils/env/index.js
+++ src/app/utils/env/index.js
@@ -1,15 +1,13 @@
-import dotenv from "dotenv";
-import { findUp } from "find-up";
-import { realpathSync, writeFileSync } from "fs";
+const dotenv = require("dotenv");
+const { findUpSync } = require("find-up");
+const { realpathSync, writeFileSync } = require("fs");
 
-// .env파일 탐색
-const envFilePath = await findUp(".env");
-// .env파일 내의 환경변수 정보 Object로 파싱
+// .env 파일 탐색
+const envFilePath = findUpSync(".env");
+// 환경변수 파싱
 const parsed = dotenv.config({ path: envFilePath }).parsed || {};
-
-// 클라이언트 HTML에 주입되어 있는 스크립트 파일 주소
+// public/env.js에 window._env 로 저장
 const scriptFilePath = `${realpathSync(process.cwd())}/public/env.js`;
-// 스크립트 파일에 window._env 객체에 parsed를 할당하도록 함
-writeFileSync(scriptFilePath, `window._env = ${JSON.stringify(parsed)}`);
+writeFileSync(scriptFilePath, `window._env = ${JSON.stringify(parsed)};`);
 
 console.log("env file parsed. create window._env object...");
