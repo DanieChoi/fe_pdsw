9fb0796508d2b9b95a695e7865a9650a14475602
hyun <32297817+hyunsokstar@users.noreply.github.com>
2025-04-18 18:10:23 +0900

    LOGIN_API_URL 수정 url 수정

--- public/env.js
+++ public/env.js
@@ -1,11 +1,11 @@
 // C:\nproject\fe_pdsw\public\env.js
 // public/env.js
 window.__RUNTIME_CONFIG__ = {
-    NEXT_PUBLIC_API_URL: "http://10.10.30.228/",
-    NEXT_PUBLIC_RESTAPI_URL: "http://10.10.40.145:8010/",
-    NEXT_PUBLIC_LOGIN_API_URL: "https://jedai-qa-web.nexuscommunity.net:9443/agent/loginCubeC",
-    NEXT_PUBLIC_DEPLOY_ENV: "production",
-    NEXT_PUBLIC_APP_VERSION: "2.1.0" // ✅ 추가!
+    API_URL: "http://10.10.30.228/",
+    RESTAPI_URL: "http://10.10.40.145:8010/",
+    LOGIN_API_URL: "https://jedai-qa-web.nexuscommunity.net:9443/agent/loginCubeC",
+    DEPLOY_ENV: "production",
+    APP_VERSION: "2.1.0" // ✅ 추가!
 
   };
   
