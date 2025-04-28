56680cfd81f51aa5c5f4a8a0a8ade6cb97dd4c61
DanieChoi <swchoi317@kakao.com>
2025-04-28 14:58:50 +0900

    [최상원]현재진행상태 분배호수제한설정 수정.

--- src/components/shared/layout/Footer.tsx
+++ src/components/shared/layout/Footer.tsx
@@ -516,9 +516,9 @@ export default function Footer({
     else if (announce === '/pds/campaign/maxcall-ext') {
       _message = '[분배호수 제한설정 ';
       if (command === 'INSERT') {
-        _message += `추가] 캠페인 아이디: [${campaign_id}], 상담사 아이디: [${data['agent_id']}], 최대분배호수: [${data['max_call']}]`;
+        _message += `추가] 캠페인 아이디: [${campaign_id}]`;
       } else if (command === 'UPDATE') {
-        _message += `수정] 캠페인 아이디: [${campaign_id}], 상담사 아이디: [${data['agent_id']}], 최대분배호수: [${data['max_call']}]`;
+        _message += `수정] 캠페인 아이디: [${campaign_id}]`;
       } else if (command === 'DELETE') {
         _message += `삭제] 캠페인 아이디: [${campaign_id}]`;
       }
